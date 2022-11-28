d59bada437cad495c8a35ab12594f737911b34e2
Kauan Marques da Luz <114708760+mrkaka1@users.noreply.github.com>
2022-11-27 22:04:42 -0400

    projetogokuzeira
    Projeto exemplar do projeto lampada.js porém com o ilustre goku.

---
+++ goku.js
@@ -0,0 +1,29 @@
+const turnOn = document.getElementById( 'turnOn' );
+const turnOff = document.getElementById( 'turnOff' );
+const goku = document.getElementById( 'goku' ); 
+
+function isLampBroken (){
+    return goku.src.indexOf ( 'preto' ) > -1
+}
+function gokuOn () {
+    
+        goku.src = './img/gokubr.jpg';
+}
+
+turnOn.addEventListener( 'click' , gokuOn);
+
+function Gokucalv(){
+    
+        goku.src = './img/Gokucalvo.jpg';
+    
+}
+//turnOff.addEventListener( 'click', lampOn);
+turnOff.addEventListener( 'click', Gokucalv);
+
+function gokupreto(){
+    goku.src = './img/gokupreto.jpg';
+}
+
+/*goku.addEventListener( 'mouseover', gokuOn);
+goku.addEventListener('mouseleave', Gokucalv);*/
+goku.addEventListener('dblclick', gokupreto);
